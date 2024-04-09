65a8825fc95d3aa0e6b7379f335640ffb7fade65
Michael Chan <mijoch@gmail.com>
2024-04-09 12:22:53 -0700

    Add interactive story for "First Move is X"

--- src/App.stories.tsx
+++ src/App.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import { userEvent, within } from "@storybook/test";
+
 import App from "./App";
 
 const meta: Meta<typeof App> = {
@@ -11,3 +13,13 @@ export default meta;
 type Story = StoryObj<typeof App>;
 
 export const TicTacToe: Story = {};
+
+export const FirstMoveIsX: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(
+      await canvas.findByLabelText("Open space. Column 1. Row 1")
+    );
+  },
+};
