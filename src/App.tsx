f27bdb321a60882038aad390fdd0bddf8f46ea47
Michael Chan <mijoch@gmail.com>
2024-04-09 12:15:54 -0700

    Fix accessibility violations in Square component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,98 @@
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState<string | null>(null);
+type SquareValue = string | null;
 
-  function handleClick() {
-    setValue("X");
+function Square({
+  value,
+  index,
+  onSquareClick,
+}: {
+  value: SquareValue;
+  index: number;
+  onSquareClick: MouseEventHandler<HTMLButtonElement>;
+}) {
+  function createAriaLabel(index: number, value: SquareValue) {
+    return [
+      value ? `Taken space. ${value}.` : `Open space.`,
+      `Column ${Math.floor(index % 3) + 1}.`,
+      `Row ${Math.floor(index / 3 + 1)}`,
+    ]
+      .join(" ")
+      .trim();
   }
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button
+      className="square"
+      onClick={onSquareClick}
+      aria-label={createAriaLabel(index, value)}
+    >
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(null));
+
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          index={0}
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        />
+        <Square
+          index={1}
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        />
+        <Square
+          index={2}
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          index={3}
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        />
+        <Square
+          index={4}
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        />
+        <Square
+          index={5}
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          index={6}
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        />
+        <Square
+          index={7}
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        />
+        <Square
+          index={8}
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        />
       </div>
     </>
   );
